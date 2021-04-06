163572e2b3186988ffb422769f611817e4f6cb8d
shr0oms <oooxxxooo1337@gmail.com>
2021-04-06 17:16:44 +0200

    version upd

--- adminscript.lua
+++ adminscript.lua
@@ -5,7 +5,7 @@ if not isfile("MAPrefix.lua") then
     writefile("MAPrefix.lua", "/")
     end
 local success, response = pcall(function()
-local Admin = {Prefix = readfile("MAPrefix.lua"), Name = "dogwater private admin", Version = "v0.9dALPHA"}
+local Admin = {Prefix = readfile("MAPrefix.lua"), Name = "dogwater private admin", Version = "v0.9eALPHA"}
 --cmd Bar poopoo nigger
 Be = Instance.new('BlurEffect')
 Be.Parent = game.Lighting
